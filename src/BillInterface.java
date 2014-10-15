72565e5e04045f0d8698202a6066bc90a1492a6a
cbkrunch17 <cbkrunch17@hotmail.com>
2014-10-15 12:27:20 -0400

    Basic BillMenu Interface Added

---
+++ src/BillInterface.java
@@ -0,0 +1,113 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Vector;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+
+
+public class BillInterface extends JFrame
+{
+	Dimension size = new Dimension(600,500);
+	JPanel billPanel; 
+	
+	
+	BillInterface()
+	{
+		super("Bill Interface");
+		
+		//Border
+				Border defaultBorder = new BevelBorder(BevelBorder.LOWERED);
+				
+		//Define Panels
+		billPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();				
+		JPanel foodPanel = new JPanel();
+		foodPanel.setBorder(defaultBorder);
+		foodPanel.add(new JLabel("Food Panel!"));
+
+		
+		//Prep BillPanel
+		GridBagLayout billPanelLayout = new GridBagLayout();
+		JScrollPane scrollingBillPanel = new JScrollPane(billPanel);
+		//scrollingBillPanel.setVerticalScrollBar(new JScrollBar());
+		scrollingBillPanel.setWheelScrollingEnabled(true);
+
+		billPanel.setLayout(billPanelLayout);
+		billPanel.setBorder(defaultBorder);
+	
+		//Prep billInterface
+		GridBagLayout billInterfacePanel = new GridBagLayout();
+		this.setLayout(billInterfacePanel);
+		GridBagConstraints c=new GridBagConstraints();	
+		c.weightx=1;
+		c.weighty=1;
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx=0;
+		c.gridy=0;
+		this.add(foodPanel,c);
+		c.gridx=1;
+		c.gridy=0;
+		this.add(scrollingBillPanel,c);
+		c.gridx=0;
+		c.gridy=1;
+		c.weighty =0.1;
+		this.add(bottomPanel,c);
+		
+		
+		this.pack();
+	    this.setSize(size);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		System.out.println("End BillInterface");
+	}
+	
+	@Override
+	public Dimension size()
+	{
+		return new Dimension(0,0);
+		
+	}
+	public boolean addMenuItem(String name)
+	{
+		GridBagConstraints c=new GridBagConstraints();	
+		c.gridwidth=0;
+		c.weightx=1;
+		c.weighty=1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.NORTH;
+		BillMenuItem bmi = new BillMenuItem(name);
+		c.ipady=bmi.getPreferredSize().height;
+		billPanel.add(bmi,c);
+		return true;
+	}
+
+	private void removeMenuItem(BillMenuItem bmi)
+	{
+		billPanel.remove(bmi);
+	}
+	
+	public boolean removeMenuItem(int index)
+	{
+		if (index < 0 ||  index > billPanel.getComponents().length)
+			return false;
+		
+		removeMenuItem((BillMenuItem) billPanel.getComponents()[index]);
+		return true;
+	}
+	
+}
---
+++ src/BillMenuItem.java
@@ -0,0 +1,99 @@
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+
+
+/*Written by Chris Bennett for 60-311
+@*Bill Menu Item used to show 
+ */
+ 
+
+public class BillMenuItem extends JPanel 
+{	
+	//JFrame Objects
+	JLabel JLbl_itemName;
+	JLabel JLbl_price;
+	//Size of the panel
+	
+	int CONST_HEIGHT = 50;
+	Dimension size = new Dimension(200,CONST_HEIGHT);
+	int price;
+	String name;
+	
+	
+	
+	BillMenuItem(String name)
+	{
+		this.name = name;
+		this.price = 5;
+		JLbl_itemName = new JLabel(name);
+		JLbl_price = new JLabel("$" + price);
+		GridLayout gridLayout =new GridLayout(0,2);
+		Border menuItemBorder = new BevelBorder(BevelBorder.RAISED);
+		
+		this.setBorder(menuItemBorder);
+		this.add(JLbl_itemName);
+		this.add(JLbl_price);
+		this.setLayout(gridLayout);	
+		this.setPreferredSize(size);
+	    this.setSize(size);
+		this.setVisible(true);
+		this.addMouseListener(new MouseListener(){
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("You clicked on "+ ((BillMenuItem) e.getComponent()));
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
+		});
+	}
+	public int getPrice()
+	{
+		return price;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	@Override
+	public String toString()
+	{
+		return "Name: " + name;
+	}
+	
+	
+	
+}
