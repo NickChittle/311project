9fe504b7f68d5723311bd44241559fd082619bf5
Nicholas Chittle <nichittle@gmail.com>
2014-11-02 01:58:33 -0400

    Made taxmanager more testable

--- src/project/TaxManager.java
+++ src/project/TaxManager.java
@@ -16,25 +16,35 @@ public class TaxManager implements Observer {
   public void addBillMenuItem(BillMenuItem i) {
     menuItems.add(i);
     i.subscribe(this);
-    calculateTax();
+    updateTaxLabel();
   }
 
-  public void calculateTax() {
+  public void updateTaxLabel() {
+    double subtotal = getSubtotal();
+    double tax = calculateTax(subtotal);
+    double total = subtotal + tax;
+
+    String text = String.format("Subtotal: $%.2f, Tax: $%.2f, Total $%.2f", subtotal, tax, total);
+    label.setText(text);
+  }
+
+  public double getSubtotal() {
     double subtotal = 0;
     for (BillMenuItem i : menuItems) {
       subtotal += i.getPrice();
     }
-    double tax = 0;;
+    return subtotal;
+  }
+
+  public double calculateTax(double subtotal) {
+    double tax = 0;
     if (subtotal >= 5) {
       tax = 0.13 * subtotal;
     }
-    double total = tax + subtotal;
-
-    String text = String.format("Subtotal: %.2f, Tax: %.2f, Total %.2f", subtotal, tax, total);
-    label.setText(text);
+    return tax;
   }
 
   public void update() {
-    calculateTax();
+    updateTaxLabel();
   }
 }
