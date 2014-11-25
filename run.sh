aaf1fa1575341929ddcaf08f759a0349192b1385
Nicholas Chittle <nichittle@gmail.com>
2014-11-24 20:05:57 -0500

    Made run shells give errors if ant is not installed

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-ant run
+if which ant > /dev/null; then
+  ant run
+else
+  echo "Please install ant: 'sudo apt-get install ant'"
+fi
--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,5 @@
-ant junit
+if which ant > /dev/null; then
+  ant junit
+else
+  echo "Please install ant: 'sudo apt-get install ant'"
+fi
